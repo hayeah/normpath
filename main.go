a725c4d3cb92918b929940bfd295fc6fdfc68b0d
Howard Yeh <howard@metacircus.com>
2017-12-27 11:42:48 +0800

    resolve path to absolute path

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func help() {
+	fmt.Println(
+		`Usage: normpath <path>
+
+Expand relative path to absolute path. Resolve symbolic links.
+`)
+
+	os.Exit(1)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Invalid usage")
+		help()
+	}
+
+	pathname := os.Args[1]
+
+	abspath, err := filepath.Abs(pathname)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(abspath)
+}
